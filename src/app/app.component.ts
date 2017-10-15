17ab02e87b2b411fcf6c0addf927c416ee97dfe5
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2017-10-15 22:11:55 +0200

    ngswitchcase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import {Component} from '@angular/core';
 export class AppComponent {
   title = 'Angular app';
   courses = [1, 2];
+  viewMode = 'sthElse';
 
   coursesAreAvailable(): boolean {
     return this.courses.length > 0;
