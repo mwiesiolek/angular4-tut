cf7b118042731edc84b9647fd9c3096a26f30360
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2017-10-15 22:24:22 +0200

    ngfor change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,4 +17,17 @@ export class AppComponent {
   coursesAreAvailable(): boolean {
     return this.courses.length > 0;
   }
+
+  onAdd(): void {
+    this.courses.push({id: 4, name: 'course4'});
+  }
+
+  onRemove(course): void {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+
+  onUpdate(course): void {
+    course.name = 'UPDATED';
+  }
 }
