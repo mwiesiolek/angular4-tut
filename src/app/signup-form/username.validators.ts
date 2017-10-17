7758f1e5cfb4381cb9facf00551eebcfab4e0b88
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2017-10-17 11:16:29 +0200

    asynchronous validation

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -2,10 +2,22 @@ import {AbstractControl, ValidationErrors} from "@angular/forms";
 
 export class UsernameValidators {
   static cannotContainSpace(c: AbstractControl): ValidationErrors | null {
-    if((c.value as String).indexOf(' ') >= 0) {
-      return { cannotContainSpace: true }
+    if ((c.value as String).indexOf(' ') >= 0) {
+      return {cannotContainSpace: true}
     }
 
     return null;
+  }
+
+  static shouldBeUnique(c: AbstractControl): Promise<ValidationErrors | null> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (c.value === 'maks') {
+          resolve({shouldBeUnique: true});
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
   }
 }
