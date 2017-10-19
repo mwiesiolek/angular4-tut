c7f32de6ca8e574060f7e2eae0e1972189488d3f
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2017-10-19 12:56:13 +0200

    combined observables

--- src/app/git-follower/git-follower.component.ts
+++ src/app/git-follower/git-follower.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit, ViewEncapsulation} from "@angular/core";
 import {GitFollowerService} from "./git-follower.service";
+import {ActivatedRoute} from "@angular/router";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'git-follower',
@@ -10,12 +12,21 @@ import {GitFollowerService} from "./git-follower.service";
 export class GitFollowerComponent implements OnInit {
   followers: any[];
 
-  constructor(private service: GitFollowerService) {
+  constructor(private route: ActivatedRoute,
+              private service: GitFollowerService) {
   }
 
   ngOnInit(): void {
-    this.service.getAll()
-      .subscribe(result => this.followers = result);
-  }
+    Observable.combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap,
+    ]).subscribe(combined => {
+      let id = +combined[0].get('id');
+      let page = +combined[1].get('page');
+      let order = combined[1].get('order');
 
+      // this.service.getAll({id: id, page: page})
+      this.service.getAll().subscribe(result => this.followers = result);
+    });
+  }
 }
